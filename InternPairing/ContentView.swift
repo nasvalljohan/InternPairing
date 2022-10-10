77cf20fae0a141a30b1cb7a049bc4e5fc9fd6ac0
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-10 13:42:38 +0200

    add: initial loginPage

--- InternPairing/ContentView.swift
+++ InternPairing/ContentView.swift
@@ -12,8 +12,37 @@ struct ContentView: View {
     }
 }
 
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("E-mail:")
+                TextField("", text: $email)
+                    .keyboardType(.emailAddress)
+                    .textFieldStyle(.roundedBorder)
+                Text("Password:")
+                SecureField("", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button(action: {
+                    // sign up
+                }, label: {
+                    Text("Sign up")
+                        .padding()
+                        .frame(width: 150)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(7)
+                })
+            }.padding()
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
