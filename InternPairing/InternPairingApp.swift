9687385bcccbd1b9c8ddedae62116c241f5a566f
Johan Näsvall <89924287+nasvalljohan@users.noreply.github.com>
2022-10-10 11:55:23 +0200

    Initial Commit

---
+++ InternPairing/InternPairingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternPairingApp.swift
+//  InternPairing
+//
+//  Created by Johan Näsvall on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct InternPairingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
