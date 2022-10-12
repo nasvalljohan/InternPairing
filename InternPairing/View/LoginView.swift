31c9dc639c4dc448b36bafae989aaf6299692c62
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-12 11:22:31 +0200

    add: toggle loginPage & noAccountPage
    Login page shows first, if clicking sign-up-button first signup page shows, if clicking login-button, login page shows.

--- InternPairing/View/LoginView.swift
+++ InternPairing/View/LoginView.swift
@@ -2,64 +2,22 @@
 import SwiftUI
 import Firebase
 
+// MARK: LoginView
 struct LoginView: View {
     @StateObject var dataManager = DataManager()
-    var authentication = Authentication()
-    @State private var email = ""
-    @State private var password = ""
+    @State var authentication = Authentication()
+    @State var isNotAUser: Bool = false
     @Binding var isUserLoggedIn: Bool
     
     var body: some View {
         ZStack{
             VStack {
-                Spacer()
-                
-                VStack {
-                    //Email
-                    Text("E-mail:")
-                    TextField("", text: $email)
-                        .keyboardType(.emailAddress)
-                        .textFieldStyle(.roundedBorder)
-                    //PW
-                    Text("Password:")
-                    SecureField("", text: $password)
-                        .textFieldStyle(.roundedBorder)
-                    
-                    //Login btn
-                    Button(action: {
-                        authentication.loginUser(email: email, password: password)
-                    }, label: {
-                        Text("Login")
-                            .padding()
-                            .frame(width: 150)
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(7)
-                    })
+                if !isNotAUser {
+                    AccountView(isNotAUser: $isNotAUser, authentication: $authentication)
+                } else {
+                    NoAccountView(isNotAUser: $isNotAUser)
                 }
                 
-                Spacer()
-                
-                VStack {
-                    Text("Not an user?")
-                    Text("I'm a...")
-                    HorizontalRadioGroupLayout()
-                    
-                    //Signup btn
-                    Button(action: {
-                        //authentication.registerUser(email: email, password: password)
-                        print("hello")
-                    }, label: {
-                        Text("Sign up")
-                            .padding()
-                            .frame(width: 150)
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(7)
-                    })
-                }
-                
-                Spacer()
             }
             .padding()
             .onAppear{
@@ -73,18 +31,109 @@ struct LoginView: View {
     }
 }
 
+struct AccountView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @Binding var isNotAUser: Bool
+    @Binding var authentication: Authentication
+    var body: some View {
+        VStack {
+            Text("Login").font(.largeTitle)
+            VStack {
+                VStack (alignment: .leading) {
+                    //Email
+                    Text("E-mail:")
+                    TextField("", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textFieldStyle(.roundedBorder)
+                    //PW
+                    Text("Password:")
+                    SecureField("", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                }
+                .padding()
+                
+                
+                //Login btn
+                Button(action: {
+                    authentication.loginUser(email: email, password: password)
+                }, label: {
+                    Text("Login")
+                        .padding()
+                        .frame(width: 300)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(3)
+                })
+                HStack {
+                    Text("Not an user?")
+                    Button(action: {
+                        isNotAUser = true
+                    }, label: {
+                        Text("Sign up")
+                    })
+                }
+            }
+        }
+    }
+}
 
+// MARK: NoAccountView
+struct NoAccountView: View {
+    @Binding var isNotAUser: Bool
+    var body: some View {
+        VStack() {
+            Text("Sign up").font(.largeTitle)
+            HStack {
+                Text("I'm a")
+                HorizontalRadioGroupLayout()
+            }
+            
+            //Signup btn
+            Button(action: {
+                //authentication.registerUser(email: email, password: password)
+                print("hello")
+            }, label: {
+                Text("Sign up")
+                    .padding()
+                    .frame(width: 300)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(3)
+            })
+            HStack {
+                Text("Already an user?")
+                Button(action: {
+                    isNotAUser = false
+                }, label:{
+                    Text("Login")
+                })
+            }
+        }
+    }
+}
+
+// MARK: HorizontalRadioGroupLayout
 struct HorizontalRadioGroupLayout: View {
     @State private var selected = 1
     var body: some View {
-        VStack(spacing: 20) {
+        VStack {
             Picker(selection: $selected, label: Text("Favorite Color")) {
                 Text("Student").tag(1)
                 Text("Recruiter").tag(2)
             }
-            .pickerStyle(.segmented)
+            .pickerStyle(.wheel)
+            .frame(width: 150)
+            .offset(x: -20)
         }
-        .padding(20)
-        .border(Color.gray)
+//        .border(Color.gray)
+    }
+}
+
+// MARK: Preview
+struct LoginView_Previews: PreviewProvider {
+    @Binding var isUserLoggedIn: Bool
+    static var previews: some View {
+        LoginView(isUserLoggedIn: .constant(true))
     }
 }
