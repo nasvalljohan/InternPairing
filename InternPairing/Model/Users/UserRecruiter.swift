adba54444e41330667244a988482e5787877415e
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-12 14:10:07 +0200

    Update UserRecruiter.swift

--- InternPairing/Model/Users/UserRecruiter.swift
+++ InternPairing/Model/Users/UserRecruiter.swift
@@ -3,7 +3,7 @@ import FirebaseFirestoreSwift
 
 struct UserRecruiter: Codable, Identifiable {
     @DocumentID var id: String?
-//    var name: String
+    var name: String
     var location: String
     var email: String
     var phoneNumber: Int
