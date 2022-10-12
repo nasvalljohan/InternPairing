6a18fa1a8a0d00dc57fcd5540c27ccd85db7cc53
Johan Näsvall <89924287+nasvalljohan@users.noreply.github.com>
2022-10-12 15:35:23 +0200

    TabView Added
    Dont know if it works

--- InternPairing/View/SwipeView.swift
+++ InternPairing/View/SwipeView.swift
@@ -4,19 +4,8 @@ struct SwipeView: View {
     @EnvironmentObject var dataManager: DataManager
     
     var body: some View {
-        NavigationView {
-//            List(dataManager.userInterns, id: \.id) { user in
-//                Text(user.name)
-//
-//            }
-//            .navigationTitle("UserInterns")
-//            .navigationBarItems(trailing: Button(action: {
-//                // add
-//            }, label: {
-//                Image(systemName: "plus")
-//            }))
-        }
-        
+        //SwipeViewStudent()
+        SwipeViewRecruiter()
     }
 }
 
@@ -25,3 +14,74 @@ struct SwipeView_Previews: PreviewProvider {
         SwipeView()
     }
 }
+
+enum NavigationType: String, Hashable {
+    case swipe = "SWIPE VIEW"
+    case contact = "CONTACT VIEW"
+    case profile = "PROFILE VIEW"
+}
+
+
+struct SwipeViewRecruiter: View {
+    @State var mainStack: [NavigationType] = []
+    
+    var body: some View {
+        NavigationStack(path: $mainStack){
+            TabView{
+                Text("Home")
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                    }
+                Text("Swipe")
+                    .tabItem{
+                        Image(systemName: "suit.heart.fill")
+                    }
+                    .onTapGesture{
+                        mainStack.append(.swipe)
+                    }
+                Text("Profile")
+                    .tabItem{
+                        Image(systemName: "person.circle")
+                    }
+                Text("Contacts")
+                    .tabItem{
+                        Image(systemName: "message.fill")
+                    }
+                
+            }
+            .navigationTitle("Jinder")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationDestination(for: NavigationType.self) { value in
+                switch(value){
+                case .swipe: SignUpView()
+                case .contact: Text("Contact view")
+                case .profile: Text("Profile view!! ")
+                }
+            }
+        }
+    }
+}
+
+struct SwipeViewStudent: View {
+    @State var mainStack: [NavigationType] = []
+    
+    var body: some View {
+        NavigationStack(path: $mainStack){
+            TabView{
+                Text("Home").tabItem {
+                    Image(systemName: "house.fill")
+                }
+                Text("Profile").tabItem{
+                    Image(systemName: "person.circle")
+                }
+                Text("Contacts").tabItem{
+                    Image(systemName: "message.fill")
+                }
+            }
+            .navigationTitle("Jinder")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+
