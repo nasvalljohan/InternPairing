cf8d669ae2775fe29c946fbf6e9926f82ca3a8a4
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-18 16:07:37 +0200

    add view: UserDetailsView
    InternView
    
    Co-Authored-By: Johan Näsvall <89924287+nasvalljohan@users.noreply.github.com>

---
+++ InternPairing/View/UserDetailsView.swift
@@ -0,0 +1,108 @@
+//
+//  UserDetailsView.swift
+//  InternPairing
+//
+//  Created by Saiman Chen on 2022-10-18.
+//
+
+import SwiftUI
+
+struct UserDetailsView: View {
+    var body: some View {
+        InternDetailsView()
+    }
+}
+
+struct InternDetailsView: View {
+    @State var description = ""
+    @State var linkedIn = ""
+    @State var location = ""
+    @State var github = ""
+    @State var selectedPlatform = 0
+    @State var selectedPosition = 0
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Profile Information")
+                    .font(.largeTitle)
+                    .padding()
+                
+                VStack(alignment: .leading) {
+                    Text("Description:")
+                    TextEditor(text: $description)
+                        .frame(
+                            height: UIScreen.main.bounds.height * 0.25
+                        ).border(.teal)
+                    
+                    Text("LinkedIn:")
+                    TextField("", text: $linkedIn)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Text("Github:")
+                    TextField("", text: $github)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Text("Location:")
+                    TextField("", text: $location)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                Spacer()
+                
+                VStack {
+                    GeometryReader { geometry in
+                        
+                        HStack(spacing: 0) {
+                            Picker(
+                                selection: $selectedPlatform, label: Text("I am")) {
+                                    Text("I am a..").tag(0)
+                                    Text("Android Dev").tag(1)
+                                    Text("iOS Dev").tag(2)
+                                    Text("React Native Dev").tag(3)
+                                }
+                                .pickerStyle(.menu)
+                                .frame(
+                                    width: geometry.size.width/2,
+                                    height: geometry.size.height,
+                                    alignment: .center)
+                                .compositingGroup()
+                                .clipped()
+                            
+                            Picker(
+                                selection: $selectedPosition,
+                                label: Text("Looking for:")) {
+                                    Text("looking for..").tag(0)
+                                    Text("Fullstack").tag(1)
+                                    Text("FrontEnd").tag(2)
+                                    Text("BackEnd").tag(3)
+                                }
+                                .pickerStyle(.menu)
+                                .frame(
+                                    width: geometry.size.width/2,
+                                    height: geometry.size.height,
+                                    alignment: .center)
+                                .compositingGroup()
+                                .clipped()
+                        }
+                    }
+                }
+                Button(action: {
+                    print("hello")
+                }, label: {
+                    Text("Save")
+                        .padding()
+                        .frame(width: 300)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(3)
+                })
+            }.padding()
+        }
+    }
+}
+
+struct UserDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserDetailsView()
+    }
+}
