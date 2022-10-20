7bfaa701e901726527370a0f450107ae7f32aab7
Johan Näsvall <j.nasvall95@gmail.com>
2022-10-20 15:23:16 +0200

    ProfileView draft

--- InternPairing/View/ProfileView.swift
+++ InternPairing/View/ProfileView.swift
@@ -4,8 +4,9 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         VStack{
-            Text("This is the profileView")
-        }.background(.black).foregroundColor(.white)
+            //StudentProfileView()
+            RecruiterProfileView()
+        }
     }
 }
 
@@ -13,6 +14,104 @@ struct ProfileView: View {
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+    }
+}
+
+struct RecruiterProfileView: View {
+    var companyName = "Apple"
+    var description = "We make computers and produce apples, we like cake and we also like to play basketball with our daddies"
+    var linkedInLink = "https://linkedin.com"
+    var companyLink = "https://apple.com"
+    var location = "Stockholm"
+    var typeOfDeveloper =  "Android"
+    var typeOfPosition = "Frontend"
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                VStack {
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                        .border(.black)
+                    Text("\(companyName)").font(.title3).bold()
+                    Text("\(typeOfPosition) \(typeOfDeveloper)  developer").font(.subheadline).bold()
+                    HStack {
+                        Image(systemName: "square").resizable().frame(width: 20, height: 20)
+                        Image(systemName: "square").resizable().frame(width: 20, height: 20)
+                    }.font(.footnote)
+                }
+                
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text("\(description)")
+                    }.padding()
+                }
+                Spacer()
+                Button(action: {
+                    print("hello")
+                }, label: {
+                    Text("Edit profile")
+                        .frame(width: 150, height: 40)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(3)
+                })
+            }.padding()
+            
+        }
+    }
+}
+
+struct StudentProfileView: View {
+    
+    var firstName = "Johan"
+    var lastName = "Näsvall"
+    var dateOfBirth = "29"
+    var description = "Im studying my last year at Stockholm Tekniska institut as Android developer, im currently looking for internship starting in february. I am a hardworking little boy that loves to make friends and play in the mud hihi"
+    var linkedInLink = "https://linkedin.com"
+    var githubLink = "https://github.com"
+    var otherLink = "google.se"
+    var location = "Stockholm"
+    var typeOfDeveloper =  "Android"
+    var typeOfPosition = "Frontend"
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                
+                VStack {
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                        .border(.black)
+                    Text("\(firstName) \(lastName), \(dateOfBirth) \(location)").font(.title3).bold()
+                    Text("\(typeOfPosition) \(typeOfDeveloper)  developer").font(.subheadline).bold()
+                    HStack {
+                        Image(systemName: "square").resizable().frame(width: 20, height: 20)
+                        Image(systemName: "square").resizable().frame(width: 20, height: 20)
+                        Image(systemName: "square").resizable().frame(width: 20, height: 20)
+                    }.font(.footnote)
+                }
+                
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text("\(description)")
+                    }.padding()
+                }
+                Spacer()
+                Button(action: {
+                    print("hello")
+                }, label: {
+                    Text("Edit profile")
+                        .frame(width: 150, height: 40)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(3)
+                })
+            }.padding()
+            
+        }
     }
 }
 
