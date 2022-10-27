dde0d7ea8979e98fb873cfd154703f86d06a0d25
Johan Näsvall <j.nasvall95@gmail.com>
2022-10-27 10:27:00 +0200

    Successfully uploaded image
    NON DYNAMIC, ADD UID TO FILENAME NEXT
    
    Co-Authored-By: saimanchen <90385724+saimanchen@users.noreply.github.com>

--- InternPairing/Model/StorageConnection.swift
+++ InternPairing/Model/StorageConnection.swift
@@ -3,22 +3,35 @@ import FirebaseStorage
 import Foundation
 
 class StorageConnection: ObservableObject {
+    
     private var storage = Storage.storage()
     
     func uploadImage(image: Data) {
         let storageReference = storage.reference()
-        let imageReference = storageReference.child("images/1")
+        let imageReference = storageReference.child("abc") // Later UUID from user
         
         
         let uploadTask = imageReference.putData(image, metadata: nil) {
             metadata, error in
-            guard let metadata = metadata else { return }
             
-            let size = metadata.size
-            imageReference.downloadURL { (url, error) in
-                guard let downloadURL = url else { return }
-                
-                print("this is inside storage function calling you")
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            if let metadata = metadata {
+                // Success
+                let size = metadata.size
+                imageReference.downloadURL { (url, error) in
+                    
+                    guard let downloadURL = url else { return } // Check download
+                    
+                    print("---------------")
+                    print(downloadURL)
+                    print("Above is url")
+                    print("-------------")
+                    
+                    
+                }
                 
             }
             
