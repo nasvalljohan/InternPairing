dc82ad5cb0c22eb710f3a6d7c7bddec4cba7472c
Johan Näsvall <j.nasvall95@gmail.com>
2022-10-27 09:20:57 +0200

    Added package
    For firebaseStorage
    
    Co-Authored-By: saimanchen <90385724+saimanchen@users.noreply.github.com>

--- InternPairing/Model/StorageConnection.swift
+++ InternPairing/Model/StorageConnection.swift
@@ -1,8 +1,7 @@
-//
-//  StorageConnection.swift
-//  InternPairing
-//
-//  Created by Johan Näsvall on 2022-10-27.
-//
+
 
 import Foundation
+
+class StorageConnection: ObservableObject {
+    
+}
