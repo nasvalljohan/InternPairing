47e2a87a372c1fa64430e8f2b0eee579ef2bfc6d
Johan Näsvall <j.nasvall95@gmail.com>
2022-10-31 12:39:44 +0100

    Networkmanager
    Yet to be implemented

---
+++ InternPairing/Services/NetworkManager.swift
@@ -0,0 +1,32 @@
+
+import Foundation
+import Network
+
+class NetworkManager: ObservableObject {
+    
+    // TODO: Implementation.
+    
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "NetworkManager")
+    @Published var isConnected: Bool?
+    
+    init(){
+        
+        monitor.pathUpdateHandler = { path in
+            
+            
+            if path.status != .satisfied {
+                print("No Internet connection")
+                
+                self.isConnected = false
+            }
+            
+            if path.status == .satisfied {
+                print("You're successfully connected to the Internet")
+                
+                self.isConnected = true
+            }
+        }
+        monitor.start(queue: queue)
+    }
+}
