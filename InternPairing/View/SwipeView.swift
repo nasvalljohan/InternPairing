23553bf063feef3b1f543de7a55cf493743f3565
Johan Näsvall <j.nasvall95@gmail.com>
2022-11-07 13:42:50 +0100

    Added typeofdeveloper

--- InternPairing/View/SwipeView.swift
+++ InternPairing/View/SwipeView.swift
@@ -64,6 +64,7 @@ struct SwipeView: View {
 struct CardView: View {
     @State private var translation: CGSize = .zero
     private var user: TheUser
+    var typeOf = TypeOfDeveloper()
     private var onRemove: (_ user: TheUser) -> Void
     private var thresholdPercentage: CGFloat = 0.4 // when the user has draged 50% the width of the screen in either direction
     
@@ -83,7 +84,6 @@ struct CardView: View {
                 
                 VStack {
                     ZStack (alignment: .bottomLeading) {
-                        
                         
                         AsyncImage(url: URL(string: user.imageUrl ?? ""), content: {
                             pic in
@@ -106,8 +106,7 @@ struct CardView: View {
                                     .font(.title)
                                     .foregroundColor(Color(.white))
                                     .bold()
-                                //TODO: Add typeofdeveloper-variable
-                                Text("Frontend Android Developer")
+                                Text(typeOf.typeOfDev(int: user.typeOfDeveloper ?? 0))
                                     .font(.subheadline)
                                     .foregroundColor(Color(.white))
                                     .bold()
@@ -154,6 +153,7 @@ struct PopUpCardView: View {
     @EnvironmentObject var db: DataManager
     @Binding var showingSheet: Bool
     @Binding var currentIntern: TheUser?
+    var typeOf = TypeOfDeveloper()
     var makeContact: (_ currentIntern: TheUser) -> Void
     
     
@@ -200,10 +200,8 @@ struct PopUpCardView: View {
                         }.offset(x: 10).shadow(radius: 4, x: 2, y: 2)
                     }
                     VStack {
-                        
                         Text("\(currentIntern.firstName ?? "") \(currentIntern.lastName ?? "")").font(.title).bold()
-                        //TODO: add functionality to translate db-int to android dev/hybrid/ios etc
-                        Text("Android Developer").font(.title3).fontWeight(.light)
+                        Text(typeOf.typeOfDev(int: currentIntern.typeOfDeveloper ?? 0)).font(.title3).fontWeight(.light)
                         
                         ScrollView {
                             Text(currentIntern.description ?? "").font(.subheadline).fontWeight(.light).fixedSize(horizontal: false, vertical: true).padding()
