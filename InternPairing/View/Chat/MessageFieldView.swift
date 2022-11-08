858ff6a3c3ba65c61b7ced9d12a5bc8cfc2068c2
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-11-08 16:35:56 +0100

    update, restructure

--- InternPairing/View/Chat/MessageFieldView.swift
+++ InternPairing/View/Chat/MessageFieldView.swift
@@ -7,31 +7,7 @@
 
 import SwiftUI
 
-struct MessageFieldView: View {
-    @State private var message = ""
-    
-    var body: some View {
-        HStack {
-            CustomTextField(placeholder: Text("Enter your message here"), text: $message)
-            
-            Button(action: {
-                print("MESSAGE SENT!")
-                message = ""
-            }, label: {
-                Image(systemName: "paperplane.fill")
-                    .foregroundColor(Color("secondaryColor"))
-                    .padding(10)
-                    .background(Color("primaryColor"))
-                    .cornerRadius(50)
-            })
-        }
-        .padding(.horizontal)
-        .padding(.vertical, 10)
-        .background(Color("secondaryColor"))
-        .cornerRadius(20)
-        .padding()
-    }
-}
+
 
 struct MessageField_Previews: PreviewProvider {
     static var previews: some View {
@@ -39,20 +15,3 @@ struct MessageField_Previews: PreviewProvider {
     }
 }
 
-struct CustomTextField: View {
-    var placeholder: Text
-    @Binding var text: String
-    var editingChanged: (Bool) -> () = { _ in }
-    var commit: () -> () = {}
-    
-    var body: some View {
-        ZStack(alignment: .leading) {
-            if text.isEmpty {
-                placeholder
-                    .opacity(0.5)
-            }
-            
-            TextField("", text: $text, onEditingChanged: editingChanged, onCommit: commit)
-        }
-    }
-}
